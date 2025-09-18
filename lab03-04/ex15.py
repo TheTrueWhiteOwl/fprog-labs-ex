30ed95e9b4609a202a6c9b71e768c87401b6204e
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-18 17:24:39 +0100

    feat: Added lab03-04/ex15

---
+++ lab03-04/ex15.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+num = int(input("Escreva um número: "))
+capicua = num
+while num > 0:
+    num, ultimo_digito = divmod(num, 10)
+    capicua = capicua*10 + ultimo_digito
+
+print("A capicua do número é", capicua)
