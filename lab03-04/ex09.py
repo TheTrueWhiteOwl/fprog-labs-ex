02cd8deebb72b4b8697d48fbb90068e163ef7ad5
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-18 16:44:58 +0100

    feat: Added lab03-04/ex09

---
+++ lab03-04/ex09.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+num = 0
+while True:
+    dig = int(input("Escreva um dígito (um número negativo para terminar) "))
+    if dig < 0:
+        break
+    if dig // 10 != 0:
+        raise ValueError("O número fornecido não é um dígito")
+    num = num*10 + dig
+print("O número é:", num)
