df98d8d0ddb4a10286f5f9526371766a387a4714
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-18 17:52:55 +0100

    feat: Added lab03-04/ex17

---
+++ lab03-04/ex17.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+num = int(input("Escreva um inteiro: "))
+num, ultimo_digito = divmod(num, 10)
+zeros_seguidos = 0
+while num > 0:
+    num, digito = divmod(num, 10)
+    if ultimo_digito == 0 and digito == 0:
+        zeros_seguidos += 1
+    ultimo_digito = digito
+
+print("O número tem", zeros_seguidos, "zeros seguidos")
