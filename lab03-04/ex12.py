1ad1094a6f0a03344dcc2dfaddf1161ba63365b8
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-18 17:15:15 +0100

    feat: Added lab03-04/ex12

---
+++ lab03-04/ex12.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+n_max = int(input("Qual o valor de n? "))
+x = int(input("Qual o valor de x? "))
+
+n = 1
+factorial = 1
+soma = 1
+while n <= n_max:
+    factorial *= n
+    soma += x**n / factorial
+    n += 1
+print("O valor da soma é", soma)
