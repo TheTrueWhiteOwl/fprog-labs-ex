1a0e7562ef741e098c02314cd167adc754ac1aff
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-18 18:12:50 +0100

    feat: Added lab03-04/ex19

---
+++ lab03-04/ex19.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+
+n = 0
+primeiro_termo = 0
+while n < 9:
+    n += 1
+    primeiro_termo = primeiro_termo*10 + n
+    print(primeiro_termo, "x 8 +", n, "=", primeiro_termo*8 + n)
