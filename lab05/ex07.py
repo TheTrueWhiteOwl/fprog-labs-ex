8c928957889bd93dc6b1e0afa0d136cf3e45d3dc
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-19 10:13:30 +0100

    fix: lab05/ex07 mais verificação de argumentosÂ

--- lab05/ex07.py
+++ lab05/ex07.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-def valor(q: float, j: float, n: int) -> float:
+def validar_taxa_de_juros(j: float):
+    if not isinstance(j, float):
+        raise TypeError("A taxa de juros tem que ser um float")
     if j <= 0 or j >= 1:
         raise ValueError("A taxa de juros tem que ser um valor entre 0 e 1")
+
+def validar_quantia_depositada(q: int):
+    if not isinstance(q, int):
+        raise TypeError("A quantia depositada deve ser um número inteiro")
     if q < 0:
         raise ValueError("A quantia depositada não pode ser negativa")
+
+def validar_numero_anos(n: int):
+    if not isinstance(n, int):
+        raise TypeError("O número de anos deve ser um número inteiro")
+    if n < 0:
+        raise ValueError("O número de anos não pode ser negativo")
+
+
+def valor(q: int, j: float, n: int) -> float:
     return q * (1 + j)**n
 
 def duplicar(q: float, j: float) -> int:
