e946e76193819c0043891a8653e4e5f7e7bc4b66
White Owl <161154637+TheTrueWhiteOwl@users.noreply.github.com>
2025-09-21 13:02:41 +0100

    fix: address things brought up regarding lab05/ex09

--- lab05/ex09.py
+++ lab05/ex09.py
@@ -2,18 +2,20 @@
 
 def dia_da_semana(dia: int, mes: int, ano: int) -> str:
     if mes < 1 or mes > 12:
-        raise ValueError("Os meses apenas vao de 0 a 12")
+        raise ValueError("Os meses apenas vao de 1 a 12")
     elif mes == 1 or mes == 2:
         mes += 12
         ano -= 1
     seculo, ano_seculo = divmod(ano, 100)
     int_dia_semana = cong_de_zeller(dia, mes, ano_seculo, seculo)
-    return int_para_dia_semana(int_dia_semana)
+    return int_para_str_dia_semana(int_dia_semana)
 
 def cong_de_zeller(q, m, K, J) -> int:
     return (q + int((13*(m + 1))/5) + K + int(K/4) + int(J/4) - 2*J) % 7
 
-def int_para_dia_semana(i: int) -> str:
+# A seguinte função pode ser escrita de forma muito melhor usando um tuplo
+# (matéria da próxima aula)
+def int_para_str_dia_semana(i: int) -> str:
     if i == 0:
         return "sabado"
     elif i == 1:
@@ -32,5 +34,5 @@ def int_para_dia_semana(i: int) -> str:
         raise ValueError("O inteiro i tem que ser um valor de 0 a 6")
 
 if __name__ == "__main__":
-    print(dia_da_semana(18, 1, 2014))
-    print(dia_da_semana(21, 5, 2006))
+    assert dia_da_semana(18, 1, 2014) == "sabado"
+    assert dia_da_semana(21, 5, 2006) == "domingo"
